93c8d5d6e57abf0b7a2c7e48c0eaf69c7dfe5eca
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-06-13 00:02:07 +0500

    Add AI task: Elektr stantsiyasi uchun energiya iste'moli hisoblovchi dastur

---
+++ ai_topshiriqlar/Elektr_stantsiyasi_uchun_energiya_istemoli_hisoblovchi_dastu.py
@@ -0,0 +1,11 @@
+# Elektr stantsiyasi uchun energiya iste'moli hisoblovchi dastur
+# Kurs: Dasturlash / IT
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+n = int(input())
+energiya_iste'moli = []
+for i in range(n):
+    energiya_iste'moli.append(int(input()))
