89c0ab6655836272fc64c4f6ae328e4dc9fd9813
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-02 12:47:19 +0500

    Add: Akkaunt holati bayroqlari [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -0,0 +1,6 @@
+royxatdan_otgan = True
+email_tasdiqlangan = False
+bloklangan = False
+print(f"Ro'yhatdan o'tgan: {royxatdan_otgan}")
+print(f"Email tasdiqlangan: {email_tasdiqlangan}")
+print(f"Bloklangan: {bloklangan}")
