0171ed685b8dbe3ff4f56449fd3f167ee3bfe3e5
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-02 11:20:38 +0500

    Add: Profil kartasi [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,11 @@
+ism = "Aziz"
+familiya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+print("=== PROFIL ===")
+print("Ism:", ism)
+print("Familiya:", familiya)
+print("Kurs:", kurs)
+print("Daraja:", daraja)
+print("Shahar:", shahar)
