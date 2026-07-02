ccf70ab7c858719722c82509556811f3fefbbc89
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-02 13:43:36 +0500

    Add: Haftalik davomat bayroqlari [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
@@ -0,0 +1,10 @@
+dushanba = True
+seshanba = True
+chorshanba = False
+payshanba = True
+juma = False
+print(f"Dushanba: {dushanba}")
+print(f"Seshanba: {seshanba}")
+print(f"Chorshanba: {chorshanba}")
+print(f"Payshanba: {payshanba}")
+print(f"Juma: {juma}")
