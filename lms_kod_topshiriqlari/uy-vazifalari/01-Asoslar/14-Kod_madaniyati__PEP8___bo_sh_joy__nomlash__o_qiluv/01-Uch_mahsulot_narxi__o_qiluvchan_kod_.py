f4569a37c1fcdb08e94e053aa3fccb41d624bbc3
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-03 09:47:03 +0500

    Add: Uch mahsulot narxi (o'qiluvchan kod) [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Uch_mahsulot_narxi__o_qiluvchan_kod_.py
@@ -0,0 +1,5 @@
+birinchi_mahsulot = int(input())
+ikkinchi_mahsulot = int(input())
+uchinchi_mahsulot = int(input())
+umumy_summa = birinchi_mahsulot + ikkinchi_mahsulot + uchinchi_mahsulot
+print(umumy_summa)
