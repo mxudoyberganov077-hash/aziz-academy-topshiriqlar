984dcebb88f9d960f66fcf571e63a6da31af4be2
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-04 15:20:04 +0500

    Add: Mahsulot cheki [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,8 @@
+nomi = input()
+narxi = int(input())
+soni = int(input())
+jami = narxi * soni
+print(f"Mahsulot: {nomi}")
+print(f"Narxi: {narxi} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami} som")
