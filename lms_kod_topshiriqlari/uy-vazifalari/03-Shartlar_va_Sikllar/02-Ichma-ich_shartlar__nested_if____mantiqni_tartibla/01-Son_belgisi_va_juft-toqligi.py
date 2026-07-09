dab8f89d9cb81c48da09c718558f61c327e4dbc0
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-09 12:30:09 +0500

    Add: Son belgisi va juft-toqligi [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Son_belgisi_va_juft-toqligi.py
@@ -0,0 +1,12 @@
+son = int(input())
+if son > 0:
+    if son % 2 == 0:
+        print("Musbat juft")
+    else:
+        print("Musbat toq")
+elif son < 0:
+    print("Manfiy")
+else:
+    print("Nol")
+    
+    
