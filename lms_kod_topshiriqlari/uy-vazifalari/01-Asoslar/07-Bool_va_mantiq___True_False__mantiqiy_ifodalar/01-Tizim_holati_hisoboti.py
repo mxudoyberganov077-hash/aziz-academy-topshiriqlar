5e958a9253bc2ce452cc15b6e5b479ef458a9471
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-09 12:37:37 +0500

    Add: Tizim holati hisoboti [mxudoyberganov077-hash]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
@@ -1,9 +1,5 @@
-server_ishlayapti = True
-baza_ulangan = True
-xatolik_bor = False
-xavfsiz = True
-print("=== TIZM HOLATI ===")
-print(f"Server ishlayapti: {server_ishlayapti}")
-print(f"Baza ulangan: {baza_ulangan}")
-print(f"Xatolik bor: {xatolik_bor}")
-print(f"Xavfsiz: {xavfsiz}")
+print("=== TIZIM HOLATI ===")
+print(f"Server ishlayapti: True")
+print(f"Baza ulangan: True")
+print(f"Xatolik bor: False")
+print(f"Xavfsiz: True")
