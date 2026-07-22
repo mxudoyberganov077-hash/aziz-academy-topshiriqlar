c25569774866fa59924b6310bcc90fbc6ccb2c09
mxudoyberganov077-hash <mxudoyberganov077@gmail.com>
2026-07-22 12:07:48 +0500

    Add: Ballni bahoga aylantirish [mxudoyberganov077-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Ballni_bahoga_aylantirish.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if score < 0 or score > 100:
+    print("notogri")
+elif score >= 86:
+    print(5)
+elif score >= 71:
+    print(4)
+elif score >= 56:
+    print(3)
+else:
+    print(3)
